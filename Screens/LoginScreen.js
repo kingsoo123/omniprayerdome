6212bca69a54bf4c2612fcd6ac751ad49cc1cdf7
kingsleyonyelo <sabistore12@gmail.com>
2023-08-07 14:54:39 +0100

    new msgg

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -144,7 +144,7 @@ const LoginScreen = ({ navigation }) => {
               marginTop: 40,
             }}
           >
-            <Text style={{ fontWeight: "500" }}>Don't have an ?</Text>
+            <Text style={{ fontWeight: "500" }}>Don't have an account?</Text>
             <TouchableOpacity
               onPress={() => navigation.navigate("RegisterScreen")}
             >
